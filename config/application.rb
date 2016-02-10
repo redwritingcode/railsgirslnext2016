a17b085c79988e2d3ed49ef864ecd36e1eabc9fc
RedWritingCode <redwritingcode@gmail.com>
2016-02-10 00:26:29 +0000

    Setup and configuration

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,10 @@
 require 'bundler'
 Bundler.require
 $:<<File.expand_path('../',__FILE__)
-Dir['./app/**/*.rb].sort.each{ |file| require file}
+Dir['./app/**/*.rb'].sort.each{ |file| require file}
 
 # configure sinatra
 set :root, Dir[',/app']
-set :public_folder, Proc.new{File.join(root,'assets')}
-set :erb, :layout => 'layouts/application'
+set :public_folder, Proc.new {File.join(root,'assets')}
+set :erb, :layout => :'layouts/application'
+
